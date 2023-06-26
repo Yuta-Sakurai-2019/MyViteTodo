0d3bfa684ab8cb8dc9d664dc3da1c55ad93153ce
YutaSakurai2023 <mogle1989@gmail.com>
2023-06-27 00:53:39 +0900

    ローカルストレージの値を出力するよう修正

--- src/components/MainTodo.vue
+++ src/components/MainTodo.vue
@@ -2,6 +2,8 @@
 import { ref } from 'vue'
 const todoRef = ref('');
 const todoListRef = ref([]);
+const ls = localStorage.todoList;
+todoListRef.value = ls ? JSON.parse(ls) : [];
 const addTodo = () => {
     // IDを簡易的にミリ秒で登録する
     const id = new Date().getTime();
@@ -18,34 +20,78 @@ const addTodo = () => {
 
 <template>
     <div class="box_input">
-        <input 
-            type="text" 
-            class="todo_input" 
+        <input
+            type="text"
+            class="todo_input"
             v-model="todoRef"
-            placeholder="+ TODOを入力" 
+            placeholder="+ TODOを入力"
         />
         <button class="btn" @click="addTodo">追加</button>
+    </div>
+    <div class="box_list">
+        <div class="todo_list" v-for="todo in todoListRef" :key="todo.id">
+            <div class="todo">
+                <input type="checkbox" class="check" /><label>{{ todo.task }}</label>
+            </div>
+            <div class="btns">
+                <button class="btn green">編</button>
+                <button class="btn pink">削</button>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-    .box_input {
-        margin-top: 20px;
-    }
-    .todo_input {
-        width: 300px;
-        margin-right: 8px;
-        padding: 8px;
-        font-size: 18px;
-        border: 1px solid #aaa;
-        border-radius: 6px;
-    }
-    .btn {
-        padding: 8px;
-        background-color: #03a9f4;
-        border-radius: 6px;
-        color: #fff;
-        text-align: center;
-        font-size: 14px;
-    }
+.box_input {
+    margin-top: 20px;
+}
+.todo_input {
+    width: 300px;
+    margin-right: 8px;
+    padding: 8px;
+    font-size: 18px;
+    border: 1px solid #aaa;
+    border-radius: 6px;
+}
+.btn {
+    padding: 8px;
+    background-color: #03a9f4;
+    border-radius: 6px;
+    color: #fff;
+    text-align: center;
+    font-size: 14px;
+}
+.box_list {
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+}
+
+.todo_list {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+}
+.todo {
+    border: 1px solid #ccc;
+    border-radius: 6px;
+    padding: 12px;
+    width: 300px;
+}
+.check {
+    border: 1px solid red;
+    transform: scale(1.6);
+    margin: 0 16px 2px 6px;
+}
+.btns {
+    display: flex;
+    gap: 4px;
+}
+.green {
+    background-color: #00c853;
+}
+.pink {
+    background-color: #ff4081;
+}
 </style>
